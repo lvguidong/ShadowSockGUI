607d8126cd7f6ef8a31712473474ffa5f0a3fc13
lvguidong <lvguidong1@gmail.com>
2026-05-17 19:57:57 +0800

    fix:single instance

--- Sources/SSMenuApp/main.swift
+++ Sources/SSMenuApp/main.swift
@@ -1,5 +1,13 @@
 import Cocoa
 
+let bundleID = "com.guidong.SSMenuApp"
+let runningApps = NSRunningApplication.runningApplications(withBundleIdentifier: bundleID)
+let myPID = ProcessInfo.processInfo.processIdentifier
+if let existing = runningApps.first(where: { $0.processIdentifier != myPID }) {
+    existing.activate(options: .activateIgnoringOtherApps)
+    exit(0)
+}
+
 let app = NSApplication.shared
 let delegate = AppDelegate()
 app.delegate = delegate
